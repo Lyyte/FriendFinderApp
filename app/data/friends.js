beec9a4ee46002120034f529d257fa608dc2faf0
Lyyte <lyytefaridi@gmail.com>
2020-04-25 17:43:07 -0500

    added directories, set up html files without javascript code, began server.js file, created friends module with friends array

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendarray = [
+    {
+        "name": "Aragorn",
+        "photo": "https://static.thetoptens.com/img/lists/15405lg.jpg",
+        "scores": [
+            3,
+            4,
+            5,
+            1,
+            3,
+            2,
+            1,
+            3,
+            2,
+            2
+        ]
+    },
+]
+
+module.exports = friendArray;
