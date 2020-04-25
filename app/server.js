28f577b11d01476874d077511fab4b438d8ab548
Lyyte <lyytefaridi@gmail.com>
2020-04-25 18:54:24 -0500

    deleted double server.js

--- app/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
-});
